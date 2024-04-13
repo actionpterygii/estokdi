f14004fb2d520846264f8a88c097cef71d3df327
actionpterygii <actionpterygii@gmail.com>
2024-04-13 14:53:03 +0900

    tsなしでやりなおし

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,1 @@
 /// <reference types="astro/client" />
-
-// declare module './json/team.json' { // 指定したいJSONファイルのpath
-//     type JSONType {
-//       // 指定したい型
-//     }
-  
-//     const value: JSONType;
-//     export = value;
-//   }
