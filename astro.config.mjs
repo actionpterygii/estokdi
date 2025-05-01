be3ed1f751782284a398b8976af0642bfd0d0627
actionpterygii <actionpterygii@gmail.com>
2025-05-01 18:37:45 +0900

    ro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://estl.actionpterygii.com",
   integrations: [sitemap({
-    lastmod: new Date('2025-04-17')
+    lastmod: new Date('2025-05-01')
   })]
 });
