8d34db0da262ffc569700e01e68e69f15ef97c2a
actionpterygii <actionpterygii@gmail.com>
2025-12-13 12:17:40 +0900

    zb isfp

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://estl.actionpterygii.com",
   integrations: [sitemap({
-    lastmod: new Date('2025-12-09')
+    lastmod: new Date('2025-12-13')
   })]
 });
